899ce56e222f795c705aabacb1b2d04422610dc6
faaez <faaez.ulhaq@gmail.com>
2014-04-02 17:49:40 -0400

    Update crossfilter.js

--- app/assets/javascripts/services/crossfilter.js
+++ app/assets/javascripts/services/crossfilter.js
@@ -7,7 +7,7 @@ angular.module('birdwatch.services').factory('cf', function (utils) {
     // crossfilter object: browser side analytics library, holds array type data (w/incremental updates).
     // dimensions are fast queries on data, e.g. view sorted by followers_count or retweet_count of the original message
     var cf = crossfilter([]);
-    var timeDim = cf.dimension(function(t) { t.mstime = moment.utc(t.datetime+" +0800")._d.getTime(); return t.mstime; });
+    var timeDim = cf.dimension(function(t) { if(t.mstime==undefined){t.mstime = moment.utc(t.datetime+" +0800")._d.getTime();} return t.mstime; });
     var tweetIdDim   = cf.dimension(function(t) { return t.id; });
     var followersDim = cf.dimension(function(t) { return t.user_followers_count; });
     var retweetsDim  = cf.dimension(function(t) {
@@ -21,7 +21,7 @@ angular.module('birdwatch.services').factory('cf', function (utils) {
 
     // Higher-order function, returns a function that rounds time down. Interval s is specified in seconds.
     // Example: returned function makes Jan 1, 2012, 16:45:00 out of Jan 1, 2012, 16:45:55 when interval is 60s
-    function dateRound(s) { return function(t) { return s * Math.floor(t.mstime / (s * 1000)) }; }
+    function dateRound(s) { if(t.mstime==undefined){t.mstime = moment.utc(t.datetime+" +0800")._d.getTime();} return function(t) { return s * Math.floor(t.mstime / (s * 1000)) }; }
 
     var byMinGrp   = cf.dimension(dateRound(       60 )).group();
     var by15MinGrp = cf.dimension(dateRound(    15*60 )).group();
